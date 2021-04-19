29e54b790fd9153343cc5f5a615394fe8d0f01b2
Daniel Hanson <hansondr@gmail.com>
2021-04-19 12:10:30 -0600

    Update submission script

--- zip_solution.sh
+++ zip_solution.sh
@@ -4,6 +4,13 @@ BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ..
 
 tar -zcvf solution.tar.gz --exclude="$BASEDIR/.git" $BASEDIR
-mv solution.tar.gz $BASEDIR
+mv solution.tar.gz "$BASEDIR/frontend"
+cd $BASEDIR
 
-echo 'solution.tar.gz has been written to the project root.'
+# This is a hacky solution to bypass Gmail's native tar/gz file scanning
+docker-compose run --rm runner gpg -c --batch --passphrase password solution.tar.gz
+
+mv "$BASEDIR/frontend/solution.tar.gz.gpg" "$BASEDIR/solution.tar.gz.gpg" \
+  && rm "$BASEDIR/frontend/solution.tar.gz"
+
+echo 'solution.tar.gz.gpg has been written to the project root.'
